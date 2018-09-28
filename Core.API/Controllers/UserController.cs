d98985ac4d8cbd6217555a3ae777e12f69beb905
fan.liao <fan.liao@hlifetech.com>
2018-09-28 14:40:15 +0800

    添加用户控制器

---
+++ Core.API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.API.Controllers
+{
+    /// <summary>
+    /// 用户控制器
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// 保存
+        /// </summary> 
+        [HttpPost]
+        public int Save()
+        {
+            return 1;
+        }
+
+        /// <summary>
+        /// 获取
+        /// </summary> 
+        [HttpGet]
+        public string Get()
+        {
+            return "{ \"name\":\"zhangsan\"}";
+        }
+    }
+}
