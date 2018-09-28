a7b06d2e3c2c7c2161f73a9d095a354a58d24528
fan.liao <fan.liao@hlifetech.com>
2018-09-28 13:18:24 +0800

    添加项目文件。

---
+++ Core.API/Filter/GlobalHttpHeaderFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.API.Filter
+{
+    /// <summary>
+    /// 头部请求参数
+    /// </summary>
+    public class GlobalHttpHeaderFilter : IOperationFilter
+    {
+        /// <summary>
+        /// Apply
+        /// </summary> 
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            operation.Parameters = operation.Parameters ?? new List<IParameter>();
+            //MemberAuthorizeAttribute 自定义的身份验证特性标记
+            //var isAuthor = operation != null && context != null && context.ApiDescription.ControllerAttributes().Any(e => e.GetType() == typeof(MemberAuthorizeAttribute)) || context.ApiDescription.ActionAttributes().Any(e => e.GetType() == typeof(MemberAuthorizeAttribute));
+            if (true)
+            {
+                operation.Parameters.Add(new NonBodyParameter()
+                {
+                    Name = "token",
+                    In = "header", //query formData ..
+                    Description = "身份验证票据",
+                    Required = false,
+                    Type = "string"
+                });
+            }
+        }
+    }
+}
---
+++ Core.API/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.API.Filter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Core.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+         
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "接口文档说明",
+                    //Description = "RESTful API",
+                    //TermsOfService = "None",
+                    //Contact = new Contact { Name = "liaowenlongcn", Email = "liaowenlongcn@gmail.com", Url = "" }
+                });
+                 
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+                var xmlPath = Path.Combine(basePath, "Core.API.xml");
+                c.IncludeXmlComments(xmlPath);
+                c.OperationFilter<GlobalHttpHeaderFilter>();  
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "接口文档说明");  
+            });
+
+            app.UseMvc();
+        }
+    }
+}
